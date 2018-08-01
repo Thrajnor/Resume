93bdfe2c9f63026614d1aaedf65c2e5188a56b87
MarcinMac <jimrajnor@gmail.com>
2018-08-01 16:27:11 +0200

    make sheet of paper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Paper from '../components/paper';
+import Paper from '../components/paper'
+import Avatar from '@material-ui/core/Avatar'
 
 const IndexPage = () => (
   <Layout>
     <Paper>
-
+      <Avatar alt='Marcin' src={require('../images/AvatarCV.png')}/>
     </Paper>
   </Layout>
 )
