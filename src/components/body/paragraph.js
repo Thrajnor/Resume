9a57027e03ad9d433622abc9b9c4071a5549eeba
MarcinMac <jimrajnor@gmail.com>
2018-08-06 17:41:07 +0200

    extends paragraph component

--- src/components/body/paragraph.js
+++ src/components/body/paragraph.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typo from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import propTypes from 'prop-types';
 
-const paragraph = (props) => {
+const styles = theme => ({
+  subTitle: {
+    fontWeight: '400',
+    fontSize: '0.85rem',
+  },
+});
+
+const Paragraph = (props) => {
+  const { classes } = props
   return (
     <Grid item xs={Number(props.gridSize)}>
-      <Typo variant='body1'>{props.title}</Typo>
+      <Typo variant='body1'>{props.title} <em className={classes.subTitle}>{props.subTitle}</em></Typo>
       <Typo variant='subheading'>{props.subheading}</Typo>
     </Grid>
   )
 }
 
-export default paragraph
+Paragraph.propTypes = {
+  classes: propTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Paragraph)
