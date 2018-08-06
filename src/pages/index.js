03719d4528e5efd2322c5f257b6349b5503aef92
MarcinMac <jimrajnor@gmail.com>
2018-08-06 16:27:11 +0200

    introduce skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Contact from '../components/header/contact'
 import Header from '../components/header/header';
 import Paragraph from '../components/header/paragraph';
 import Divider from '../components/base/divider';
+import Skills from '../components/body/skills';
 
 const IndexPage = () => (
   <Layout>
@@ -19,13 +20,22 @@ const IndexPage = () => (
                Github='github.com/Thrajnor' 
                Home='Wrocław, Poland' 
                Phone='123 456 789'/>
-      <Paragraph gridSize='12'>Proactive and ready to work right away.</Paragraph>
-      <Divider gridSize='12' icon={<i class="fas fa-cogs"></i>} title='Skills'>
-      </Divider>
-      <Divider gridSize='12' icon={<i class="fas fa-comments"></i>} title='Languages'/>
-      <Divider gridSize='12' icon={<i class="fas fa-code"></i>} title='Work Experience'/>
-      <Divider gridSize='12' icon={<i class="fas fa-check-circle"></i>} title='Certificates'/>
-      <Divider gridSize='12' icon={<i class="far fa-handshake"></i>} title='Soft Skills'/>
+      <Paragraph gridSize='12'>i'm proactive and eager to learn new things through work.</Paragraph>
+      <Divider gridSize='12' icon={<i className="fas fa-cogs"></i>} title='Skills'/>
+        <Skills gridSize='6' skills={[
+            {icon:<i className="fab fa-html5"></i>, name: 'Html5', experience: '4'},
+            {icon:<i className="fab fa-css3-alt"></i>, name: 'Css3', experience: '4'},
+            {icon:<i className="fab fa-git-square"></i>, name: 'Git', experience: '3'},
+            {icon:<i className="fab fa-js-square"></i>, name: 'JavaScript', experience: '3'},
+            {icon:<i className="fab fa-react"></i>, name: 'React', experience: '3'},
+            {icon:<i className="fab fa-sass"></i>, name: 'Sass', experience: '3'},
+            {icon:<i className="fab fa-node-js"></i>, name: 'NodeJs', experience: '2'},
+            {icon:<i className="fab fa-vuejs"></i>, name: 'VueJS', experience: '2'},
+          ]} />
+      <Divider gridSize='12' icon={<i className="fas fa-comments"></i>} title='Languages'/>
+      <Divider gridSize='12' icon={<i className="fas fa-code"></i>} title='Work Experience'/>
+      <Divider gridSize='12' icon={<i className="fas fa-check-circle"></i>} title='Certificates'/>
+      <Divider gridSize='12' icon={<i className="far fa-handshake"></i>} title='Soft Skills'/>
     </Paper>
   </Layout>
 )
