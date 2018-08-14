59aa712cb6bea3a402a203a67c508e792fd3cce1
MarcinMac <jimrajnor@gmail.com>
2018-08-14 20:53:43 +0200

    add new avatar

--- src/components/header/avatar.js
+++ src/components/header/avatar.js
@@ -13,6 +13,7 @@ const styles = theme => ({
     width: '100%',
     height: '100%',
     margin: 0,
+    boxShadow: '0px .5px 5px 0px rgba(0, 0, 0, 0.2), 0px .25px 2px 0px rgba(0, 0, 0, 0.14), 0px .125px 1px -2px rgba(0, 0, 0, 0.12);'
   }
 });
 
