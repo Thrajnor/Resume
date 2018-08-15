3997e733fb4aef5a994138c923fe2b0908034ff3
MarcinMac <jimrajnor@gmail.com>
2018-08-15 10:57:04 +0200

    repair a sloppy bug

--- src/html.js
+++ src/html.js
@@ -12,7 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <meta name="description" content="Resume of Marcin Zaborowski" />>
+          <meta name="description" content="Resume of Marcin Zaborowski" />
           <link href="https://fonts.googleapis.com/css?family=Merienda:400,700" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" />
