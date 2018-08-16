8b8dc24c21cdefc3dee69cf0ef90547eff8995e0
MarcinMac <jimrajnor@gmail.com>
2018-08-16 16:44:59 +0200

    change to statefull

--- src/components/body/skills.js
+++ src/components/body/skills.js
@@ -3,6 +3,7 @@ import propTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Typo from '@material-ui/core/Typography';
+import Divider from 'components/base/divider';
 
 
 
@@ -22,68 +23,75 @@ const styles = theme => ({
     },
   }
 });
+class Skills extends React.Component {
 
-const Skills = (props) => {
-  const { classes } = props
-  // sorts skillsArray
-  const biggerFirst = () => {
-    let array = props.skills
-    const compare = (a,b) => {
-      if (a.experience < b.experience)
-        return 1;
-      if (a.experience > b.experience)
-        return -1;
-      return 0;
+  render() {
+    const { classes } = this.props
+    // sorts skillsArray
+    const biggerFirst = () => {
+      let array = this.props.skills
+      const compare = (a,b) => {
+        if (a.experience < b.experience)
+          return 1;
+        if (a.experience > b.experience)
+          return -1;
+        return 0;
+      }
+
+      let skillsArray = array.sort(compare)
+      
+      // skillsArray = skillsArray.reverse()
+      return skillsArray
+    }
+    let skillsArray = biggerFirst()
+
+    // make experience radio
+    const experience = (num) => {
+      let checked = [] 
+      let unchecked = []
+      if (num === '') {
+        return
+      }
+      for(let i = 0; i < num; i++) {
+        checked.push(<i className="material-icons" key={i}>radio_button_checked</i>)
+      }
+      for(let i = 0; i < (5 - num); i++) {
+        checked.push(<i className="material-icons" key={i + 5}>radio_button_unchecked</i>)
+      }
+      return (
+        <span className={classes.Experience}>
+          {checked} {unchecked}
+        </span>
+      )
     }
 
-    let skillsArray = array.sort(compare)
-    
-    // skillsArray = skillsArray.reverse()
-    return skillsArray
-  }
-  let skillsArray = biggerFirst()
+    // Fix classes
+    let fixClasses = [classes.text, classes.marginRight].join(' ')
 
-  // make experience radio
-  const experience = (num) => {
-    let checked = [] 
-    let unchecked = []
-    if (num === '') {
-      return
-    }
-    for(let i = 0; i < num; i++) {
-      checked.push(<i className="material-icons" key={i}>radio_button_checked</i>)
-    }
-    for(let i = 0; i < (5 - num); i++) {
-      checked.push(<i className="material-icons" key={i + 5}>radio_button_unchecked</i>)
-    }
+    // make list of skills
+    const skills = skillsArray.map((skill) => {
+      return (
+        <Grid item xs={Number(this.props.gridSizeXS)} sm={Number(this.props.gridSizeSM)} md={Number(this.props.gridSize)} key={skill.name}>
+          <Typo className={skill.fix === 'mr' ? fixClasses : classes.text} variant={skill.variant}>
+            {skill.icon} <span className={skill.fix}>{skill.name}</span>
+            {experience(skill.experience)}
+          </Typo>
+        </Grid>
+      )
+    })
+
+
     return (
-      <span className={classes.Experience}>
-        {checked} {unchecked}
-      </span>
+        <>
+          <Divider gridSize='12' icon={this.props.icon} title={this.props.title}/>
+          <Grid item xs={12}>
+            <Grid container spacing={0} className='slide-toggle__box-inner' style={{ opacity: Math.max(0.5, range) }}>
+              {skills}
+            </Grid>
+          </Grid>
+        </>
     )
   }
-
-  // Fix classes
-  let fixClasses = [classes.text, classes.marginRight].join(' ')
-
-  // make list of skills
-  const skills = skillsArray.map((skill) => {
-    return (
-      <Grid item xs={Number(props.gridSizeXS)} sm={Number(props.gridSizeSM)} md={Number(props.gridSize)} key={skill.name}>
-        <Typo className={skill.fix === 'mr' ? fixClasses : classes.text} variant={skill.variant}>
-          {skill.icon} <span className={skill.fix}>{skill.name}</span>
-          {experience(skill.experience)}
-        </Typo>
-      </Grid>
-    )
-  })
-  return (
-    <Grid item xs={12}>
-      <Grid container spacing={0}>
-        {skills}
-      </Grid>
-    </Grid>
-  )
 }
 
 Skills.propTypes = {
